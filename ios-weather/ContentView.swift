ad3d0ac19c29fc2a765d42b9f7d9fa089b8ea11e
Hermansyah Putra <hermansyahputra0418@gmail.com>
2024-06-17 14:46:28 +0700

    Initial Commit

---
+++ ios-weather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios-weather
+//
+//  Created by Putra on 17/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios-weather/ios_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_weatherApp.swift
+//  ios-weather
+//
+//  Created by Putra on 17/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
